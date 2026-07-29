b6f7a56550ebdba4903232d110a633cec0db53c7
Trey <88073284+Trey-Gaines@users.noreply.github.com>
2026-07-28 21:35:37 -0500

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+        var maxInt = 0
+        var curr = 0
+        var i = 0
+        let count = nums.count - 1
+
+        for i in 0...count {
+            print(i, nums[i], curr, maxInt)
+            if nums[i] == 1 {
+                curr += 1
+            } 
+            
+            if nums[i] == 0 || i == count {
+                curr == 1 ? curr += 1 : nil
+                maxInt = max(curr, maxInt)
+                curr = 0
+            }
+        }
+
+        return maxInt
+    }
+}
